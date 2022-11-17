f80bffc2b00171154af652292f9b65e033264e22
dtjgp <dtjgp92613@gmail.com>
2022-11-17 12:35:01 +0100

    week_3

--- test1.c
+++ test1.c
@@ -2,12 +2,17 @@
 
 int main()
 {
-    printf("please enter the height in foot and inch: ");
-    float foot;
-    float inch;
-    
-    scanf("%f %f", &foot, &inch);
-    printf("the height is %f feet and %f inches\n", foot, inch);
-    printf("the height is %f meter\n", ((foot + inch/12)*0.3048));
+    int a;
+    int b;
+    int c;
+    int d;
+    int f;
+    int g;
+    scanf("%d %d", &a, &b);
+    c = a/100 * 60 + a%100 + b;
+    d = c/60;
+    f = c%60;
+    g = d*100 + f;
+    printf("%d\n", g);
     return 0;
 }
