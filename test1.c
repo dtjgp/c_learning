97750e8e76091d6a3b4bb04d55609040ff4247c6
dtjgp <dtjgp92613@gmail.com>
2022-11-21 18:23:33 +0100

    week_14
    the test2.c still have bug

--- test1.c
+++ test1.c
@@ -1,8 +1,60 @@
+//resizable array
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+
+typedef struct
+{
+    int *array;
+    int size;
+    int capacity;
+} resizable_array;
+
+void init_array(resizable_array *ra, int capacity)
+{
+    ra->array = malloc(capacity * sizeof(int));
+    ra->size = 0;
+    ra->capacity = capacity;
+}
+
+void free_array(resizable_array *ra)
+{
+    free(ra->array);
+}
+
+void resize_array(resizable_array *ra, int new_capacity)
+{
+    int *new_array = malloc(new_capacity * sizeof(int));
+    memcpy(new_array, ra->array, ra->size * sizeof(int));
+    free(ra->array);
+    ra->array = new_array;
+    ra->capacity = new_capacity;
+}
+
+void add_to_array(resizable_array *ra, int value)
+{
+    if (ra->size == ra->capacity)
+    {
+        resize_array(ra, 2 * ra->capacity);
+    }
+    ra->array[ra->size++] = value;
+}
 
 int main(int argc, char const *argv[])
 {
-    printf("%9d\n", 123);
-    printf("%-9d\n", 123);
+    resizable_array ra;
+    init_array(&ra, 10);
+    for (int i = 0; i < 100; i++)
+    {
+        add_to_array(&ra, i);
+    }
+    for (int i = 0; i < ra.size; i++)
+    {
+        printf("%d ", ra.array[i]);
+    }
+    printf("\n");
+    free_array(&ra);
     return 0;
 }
+
--- test2.c
+++ test2.c
@@ -1,11 +1,97 @@
+//linked block
+
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct _node
+{ 
+    int value;
+    struct _node *next;
+} Node;
+
+typedef struct _list
+{
+    Node *head;
+    Node *tail;
+} List;
+
+void add(List* pList, int number)
+{
+    
+    //add to linked-list
+    Node *p = (Node *)malloc(sizeof(Node));
+    p->value = number;
+    p->next = NULL;
+    //find the last
+    Node *last = pList->head;
+    if ( last)
+    {
+        while ( last->next) 
+        {
+            last = last->next;
+        }
+    
+        last->next = p;
+    }
+    else
+    {
+        pList->head = p;
+    }   
+}
 
 int main(int argc, char const *argv[])
 {
-    int num;
-    int i1 = scanf("%i", &num);
-    int i2 = printf("%d\n", num);
-    printf("%d:%d\n", i1, i2);
+    List list;
+    int number;
+    list.head = list.tail = NULL;
+    do
+    {
+        scanf("%d", &number);
+        if (number != -1)
+        {
+            head = add(&list, number);
+        }
+        
+    } while (number != -1);
+    printf(&list);
+    scanf("%d", &number);
+    Node *p;
+    int isFound = 0;
+    for (p = list.head; p != NULL; p = p->next)
+    {
+        if (p->value == number)
+        {
+            isFound = 1;
+            break;
+        }
+    }
+    if (isFound)
+    {
+        printf("Found");
+    }
+    else
+    {
+        printf("Not found");
+    }
+    Node *q;
+    for (q = NULL, p = list.head; p != NULL; p = q, p = p->next)
+    {
+        if (p->value == number)
+        {
+            if(q)
+            {
+                q->next = p->next;
+            }
+            else
+            {
+                list.head = p->next;
+            }
+            free(p);
+            break;
+        }
+    }
 
+    printf("\n");
     return 0;
 }
